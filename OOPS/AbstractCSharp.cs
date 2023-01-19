3cc5eb2501fc69fd4ab532e44fcd0aa80b357421
deepakpatill <deepak.patil@atriina.com>
2023-01-19 13:56:56 +0530

    feat: abstract, class property demo

---
+++ OOPS/AbstractCSharp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    abstract class Game
+    {
+        public abstract void Display();
+    }
+    class AbstractCSharp : Game
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Abstract Demo");
+        }
+
+       public void AbstractDemo()
+        {
+            Game g = new AbstractCSharp();
+            g.Display();
+        }
+
+    }
+}
---
+++ OOPS/ClassProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Student
+    {
+        private string name;
+
+        public void setName(string s)
+        {
+            this.name = s;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+    class ClassProperty
+    {
+        public void ClassPropertyDemo()
+        {
+            Student s = new Student();
+            Console.WriteLine("Set name using setName property");
+            s.setName("ABC");
+            Console.WriteLine("Get name using getname property , Name: {0}", s.getName());
+           
+        }
+    }
+}
